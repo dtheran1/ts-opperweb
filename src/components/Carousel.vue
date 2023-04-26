503efdbcdb9f86321a6f3dbbce1b0bb406ab4d83
Dtheran <dtheran1@gmail.com>
2023-04-26 13:54:50 -0500

    add slides

---
+++ src/components/Carousel.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="carousel">
+    <slot :currentSlide="currentSlide" />
+
+    <!-- Pagination -->
+    <div class="pagination absolute bottom-10 w-full flex justify-center gap-4 items-center">
+      <span v-for="(slide, i) in getSlideCount" :key="i" @click="goToSlide(i)"
+        class="cursor-pointer w-14 h-2 rounded-full shadow-md shadow-grayCustom"
+        :class="i + 1 === currentSlide ? 'bg-gray-100' : 'bg-gray-600'">
+      </span>
+    </div>
+  </div>
+</template>
+<script>
+import { defineComponent, ref, onMounted } from 'vue'
+export default defineComponent({
+  setup() {
+    const currentSlide = ref(1)
+    const getSlideCount = ref(null)
+    const autoPlayEnabled = ref(true)
+    const timeoutDuration = ref(5000)
+
+    const nextSlide = () => {
+      if (currentSlide.value === getSlideCount.value) {
+        currentSlide.value = 1;
+        return;
+      }
+      currentSlide.value += 1;
+    };
+
+    const goToSlide = (index) => {
+      currentSlide.value = index + 1;
+    };
+
+    const autoPlay = () => {
+      setInterval(() => {
+        nextSlide();
+      }, timeoutDuration.value);
+    };
+    if (autoPlayEnabled.value) {
+      autoPlay();
+    }
+    onMounted(() => {
+      getSlideCount.value = document.querySelectorAll(".slide").length;
+    });
+
+    return {
+      currentSlide,
+      nextSlide,
+      getSlideCount,
+      goToSlide
+    }
+  }
+})
+</script>
+<style scoped></style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,7 +1,7 @@
 <template >
   <div
-    class="xs:w-[375px] xs:border-gray-100 xs:border-2 lg:w-screen lg:border-none rounded-xl lg:rounded-none grid lg:grid-cols-2 bg-gradient-to-b from-primary to-secondary min-h-screen">
-    <div class="lg:bg-black pt-11">
+    class="xs:w-[375px] md:w-full  xs:border-gray-100 xs:border-2  lg:border-none rounded-xl lg:rounded-none grid grid-cols-1 md:grid-cols-2 bg-gradient-to-b from-primary to-secondary min-h-screen">
+    <div class="md:bg-black pt-6">
       <div class="pl-[34px] pt-5 pb-6 hidden lg:flex">
         <img src="../assets/login/image 1.png" alt="logo-page">
         <span class="font-extrabold text-white self-center pl-3">anime<span class="text-primary">yabu.</span></span>
@@ -66,71 +66,63 @@
         </div>
       </div>
     </div>
-    <div class="hidden lg:block bg-complement">
-      <div id="default-carousel" class="relative w-full" data-carousel="slide">
-        <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-          <!-- Item 1 -->
-          <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="../assets/login/slider1.png"
-              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+    <div class="hidden md:block relative bg-complement">
+      <div class="w-[538px] mt-10 h-[538px] mx-auto z-10 bg-pinkCustom rounded-full absolute left-1/4 top-8" />
+      <Carousel class="carousel" v-slot="{ currentSlide }">
+        <Slide v-for="(slide, index) in carouselSlides" :key="index">
+          <div v-show="currentSlide === index + 1" class="slide-info absolute top-14 left-48 z-50">
+            <img :src="slide.slide" alt="slider" class="mx-auto" :class="currentSlide === 1 && 'h-[600px]'">
+            <p class="text-white font-bold text-center text-[80px]"> anime<span class="text-primary">{{
+              slide.title.split('anime').join('') }}.</span>
+            </p>
+            <div class="">
+              <p class="text-white text-center w-[463px] mx-auto"> {{ slide.description }} </p>
+            </div>
           </div>
-          <!-- Item 2 -->
-          <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="../assets/login/slider2.png"
-              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-          </div>
-          <!-- Item 3 -->
-          <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="../assets/login/slider3.png"
-              class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-          </div>
-        </div>
-        <!-- Slider indicators -->
-        <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-          <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-            data-carousel-slide-to="0"></button>
-          <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-            data-carousel-slide-to="1"></button>
-          <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-            data-carousel-slide-to="2"></button>
-          <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-            data-carousel-slide-to="3"></button>
-          <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-            data-carousel-slide-to="4"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button"
-          class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-          data-carousel-prev>
-          <span
-            class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
-              stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7"></path>
-            </svg>
-            <span class="sr-only">Previous</span>
-          </span>
-        </button>
-      <button type="button"
-        class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next>
-        <span
-          class="inline-flex items-center justify-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg aria-hidden="true" class="w-5 h-5 text-white sm:w-6 sm:h-6 dark:text-gray-800" fill="none"
-            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7"></path>
-          </svg>
-          <span class="sr-only">Next</span>
-        </span>
-      </button>
+        </Slide>
+      </Carousel>
     </div>
-
   </div>
-</div></template>
+</template>
 <script>
-export default {
 
-}
+import { defineComponent } from 'vue'
+import Carousel from '../components/Carousel.vue'
+import Slide from '../components/SliderComponent.vue'
+
+import slade1 from '../assets/login/slider1.png'
+import slade2 from '../assets/login/slider2.png'
+import slade3 from '../assets/login/slider3.png'
+
+export default defineComponent({
+  name: 'login',
+  components: {
+    Carousel,
+    Slide
+  },
+  setup() {
+    const carouselSlides = [
+      {
+        title: 'Yabu',
+        slide: slade1,
+        description: 'Ver anime en línea en HD, subtitulado o doblado, en tu celular o computadora.¡Animeyabu, tu portal de anime en línea!'
+      },
+      {
+        title: 'Kyojuro',
+        slide: slade2,
+        description: 'Ver anime en línea en HD, subtitulado o doblado, en tu celular o computadora.¡Animeyabu, tu portal de anime en línea!'
+      },
+      {
+        title: 'Inosuke',
+        slide: slade3,
+        description: 'Ver anime en línea en HD, subtitulado o doblado, en tu celular o computadora.¡Animeyabu, tu portal de anime en línea!'
+      },
+    ]
+    return {
+      carouselSlides
+    }
+  }
+
+})
 </script>
 <style scoped></style>
